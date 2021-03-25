8134335cb432b20f5bad38079b071a5bd397f7a4
Harshaprabeesh <39194149+Harshaprabeesh@users.noreply.github.com>
2021-03-24 22:45:55 -0400

    added save employee service class

--- src/test/java/com/test/EmployeeTest.java
+++ src/test/java/com/test/EmployeeTest.java
@@ -2,46 +2,39 @@ package com.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.models.Employees;
-import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
+import com.services.EmployeeService;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
-import java.net.URL;
-
-import static io.restassured.RestAssured.given;
 
 
 public class EmployeeTest {
 private ObjectMapper mapper  = new ObjectMapper();
-private String baseURI ="http://jsonplaceholder.typicode.com";
-private String basePath = "/posts";
+//moved to common class service
+//private String baseURI ="http://jsonplaceholder.typicode.com";
+//private String basePath = "/posts";
  @Test
     public void saveANewEmployee() {
 
         String file ="employee.json";
-        URL url = getClass().getClassLoader().getResource(file);
-     Employees employees=null;
-     try {
-        employees = mapper.readValue(url, Employees.class);
-     } catch (IOException e) {
-         System.out.println("File reading error");
-         e.printStackTrace();
-     }
-     System.out.println(employees);
-     //post
-given().baseUri(baseURI)
-             .basePath(basePath)
-             .contentType(ContentType.JSON)
-             .body(employees)
-             .log().all()
-     .when()
-             .log().all()
-             .post("/")
 
-     .then()
-             .log().all()
-             .assertThat().statusCode(HttpStatus.SC_CREATED);
+
+    EmployeeService employeeService =new EmployeeService();
+     Employees employees= employeeService.readAFile(file);
+     employeeService.saveNewEmployee(employees);
+
+
+     //post--moved to common class
+//given().baseUri(baseURI)
+//             .basePath(basePath)
+//             .contentType(ContentType.JSON)
+//             .body(employees)
+//             .log().all()
+//     .when()
+//             .log().all()
+//             .post("/")
+//
+//     .then()
+//             .log().all()
+//             .assertThat().statusCode(HttpStatus.SC_CREATED);
 
 ////GET - stores and asserts body
 //     ValidatableResponse response=given().baseUri(baseURI)
